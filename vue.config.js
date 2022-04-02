128ce1124c4cd8b049dbd109d5c0f7503fae7699
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-04-02 01:11:22 -0600

    add : ssh

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/Practica_Semana5_Vue/" : "/"
-})
+// vue.config.js file to be placed in the root of your repository
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Practica_Semana5_Vue/'
+    : '/'
+}
